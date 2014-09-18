87e7e0dcfbf50906ce3a7a1ff56fd0db9bf2109c
zhenya <zhenya@boss>
2014-09-18 22:39:18 +0300

    fix 1.1.11

--- src/adinformer/JFScanner.java
+++ src/adinformer/JFScanner.java
@@ -5,17 +5,145 @@
  */
 package adinformer;
 
+import static adinformer.ADInformer.log;
+import java.awt.Cursor;
+import java.awt.Toolkit;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+import java.net.UnknownHostException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.SwingUtilities;
+import javax.swing.SwingWorker;
+
 /**
  *
  * @author zhenya
  */
 public class JFScanner extends javax.swing.JFrame {
+    private static OutputStream out;
+    private static Scanner scan;
+    private static String ip;
+    private static String dnsname;
+    private static String username;
+    private static String fullname;
+    private static String telephonenumber;
+    private static String mobile;
+    private static String mail;
+    private static String ipphone;
+    
+    class Scanner extends SwingWorker<Void, Void> {
+        @Override
+        public Void doInBackground() {
+            redirectSystemStreams();
+            isScanner();
+            return null;
+        }
+        
+        @Override
+        public void done() {
+            Toolkit.getDefaultToolkit().beep();
+            jButton1.setEnabled(true);
+            jButton2.setEnabled(true);
+            setCursor(null);
+            setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
+            jLabel2.setText("Сканирование завершено");
+        }        
+    }
+    
+    private void updateTextArea(final String text) {                            
+        SwingUtilities.invokeLater(new Runnable() { 
+            @Override
+            public void run() {                               
+                jTextArea1.append(text);                
+            }                 
+        });                
+    }
+    
+    private void redirectSystemStreams() {  
+        out = new OutputStream() {  
+            @Override  
+                public void write(int b) throws IOException {  
+                    updateTextArea(String.valueOf((char) b));
+                }
+            @Override  
+                public void write(byte[] b, int off, int len) throws IOException {                  
+                    updateTextArea(new String(b, off, len));                
+                }
+            @Override  
+                public void write(byte[] b) throws IOException {  
+                    write(b, 0, b.length);                      
+                }                  
+        };            
+        System.setOut(new PrintStream(out, true));  
+        System.setErr(new PrintStream(out, true)); 
+    }
+    
+    public void isScanner() {
+        try {
+            System.out.println("IPv4: "+jTextField1.getText());
+            try {
+                //получаем DNS name
+                dnsname = AdUtil.getDnsName(ip);
+            } catch (UnknownHostException ex) {            
+                JOptionPane.showMessageDialog(null,"Ошибка обработки DNS name\n"+ex);
+                try {
+                    log.writeLog(ex.getMessage());                
+                } catch (IOException ex1) {
+                    JOptionPane.showMessageDialog(null,"Ошибка записи в лог файл\n"+ex1);                
+                }
+            }
+            System.out.println(" DNS name: " + dnsname);
+            //получаем пользователя
+            username = AdUtil.getUser(ip);
+            System.out.println("Login: " + username);
+            if (username.isEmpty() && username.equals("")) {                
+                username = "";            
+                System.out.println("Имя пользователя не найдено");
+            } else {
+                int in = username.indexOf("\\");
+                username = username.substring(in+1);
+                try {
+                    AdSearch.getUser(username);                                
+                } catch (NullPointerException ex) {
+                    System.out.println(ex);
+                    username = null;
+                }
+                fullname = AdSearch.getUserName();
+                telephonenumber = AdSearch.getUserTelephone();
+                mobile = AdSearch.getUserMobile();
+                mail = AdSearch.getUserMail();
+                ipphone = AdSearch.getUserIpPhone();
+            
+                System.out.println("fullname: "+fullname);
+                System.out.println("Tel: "+telephonenumber);
+                System.out.println("Mobile: "+mobile);
+                System.out.println("Mail: "+mail);
+                System.out.println("IpPhone: "+ipphone);
+                
+                
+            }
+            
+            
+        } catch (Exception ex) {
+            try {
+                JOptionPane.showMessageDialog(null,"Ошибка в модуле сканирования\n"+ex);
+                log.writeLog(ex.getMessage());                
+            } catch (IOException ex1) {
+                JOptionPane.showMessageDialog(null,"Ошибка записи в лог файл\n"+ex1);                
+            }
+        }
+    }
+    
 
     /**
      * Creates new form JFSanner
      */
     public JFScanner() {
         initComponents();
+        
     }
 
     /**
@@ -34,6 +162,7 @@ public class JFScanner extends javax.swing.JFrame {
         jTextField1 = new javax.swing.JTextField();
         jScrollPane1 = new javax.swing.JScrollPane();
         jTextArea1 = new javax.swing.JTextArea();
+        jLabel2 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.DISPOSE_ON_CLOSE);
         setTitle("Сканер IPv4");
@@ -46,12 +175,20 @@ public class JFScanner extends javax.swing.JFrame {
         });
 
         jButton2.setText("Сканировать");
+        jButton2.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton2ActionPerformed(evt);
+            }
+        });
 
         jLabel1.setText("IPv4 address:");
 
         jTextArea1.setColumns(20);
+        jTextArea1.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
         jTextArea1.setRows(5);
         jScrollPane1.setViewportView(jTextArea1);
+
+        jLabel2.setFont(new java.awt.Font("Tahoma", 0, 12)); // NOI18N
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -59,7 +196,9 @@ public class JFScanner extends javax.swing.JFrame {
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
                 .addComponent(jButton2)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jButton1))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
@@ -82,9 +221,11 @@ public class JFScanner extends javax.swing.JFrame {
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 212, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jButton1)
-                    .addComponent(jButton2)))
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                        .addComponent(jButton1)
+                        .addComponent(jButton2))
+                    .addComponent(jLabel2, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -109,9 +250,74 @@ public class JFScanner extends javax.swing.JFrame {
     }// </editor-fold>//GEN-END:initComponents
 
     private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
-        // TODO add your handling code here:
         dispose();
     }//GEN-LAST:event_jButton1ActionPerformed
+
+    private void jButton2ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton2ActionPerformed
+        try {
+            jLabel2.setText("Сканирование... подождите");
+            jButton1.setEnabled(false);
+            jButton2.setEnabled(false);
+            setCursor(Cursor.getPredefinedCursor(Cursor.WAIT_CURSOR));            
+            setDefaultCloseOperation(javax.swing.WindowConstants.DO_NOTHING_ON_CLOSE);
+            //run task
+            scan = new Scanner();
+            scan.execute();
+            
+            /**
+            System.out.println(jTextField1.getText());
+            try {
+                //получаем DNS name
+                dnsname = AdUtil.getDnsName(ip);
+            } catch (UnknownHostException ex) {            
+                JOptionPane.showMessageDialog(null,"Ошибка обработки DNS name\n"+ex);
+                try {
+                    log.writeLog(ex.getMessage());                
+                } catch (IOException ex1) {
+                    JOptionPane.showMessageDialog(null,"Ошибка записи в лог файл\n"+ex1);                
+                }
+            }
+            System.out.print(" DNS name: " + dnsname + "\t");
+            //получаем пользователя
+            username = AdUtil.getUser(ip);
+            System.out.print("Login: " + username + "\t");
+            if (username.isEmpty() && username.equals("")) {                
+                username = "";            
+                System.out.println("Имя пользователя не найдено");
+            } else {
+                int in = username.indexOf("\\");
+                username = username.substring(in+1);
+                try {
+                    AdSearch.getUser(username);                                
+                } catch (NullPointerException ex) {
+                    System.out.print(ex);
+                    username = null;
+                }
+                fullname = AdSearch.getUserName();
+                telephonenumber = AdSearch.getUserTelephone();
+                mobile = AdSearch.getUserMobile();
+                mail = AdSearch.getUserMail();
+                ipphone = AdSearch.getUserIpPhone();
+            
+                System.out.print("fullname: "+fullname+"\t");
+                System.out.print("Tel: "+telephonenumber+"\t");
+                System.out.print("Mobile: "+mobile+"\t");
+                System.out.print("Mail: "+mail+"\t");
+                System.out.print("IpPhone: "+ipphone+"\t");
+                
+                
+            }*/
+            
+            
+        } catch (Exception ex) {
+            try {
+                JOptionPane.showMessageDialog(null,"Ошибка в модуле сканирования\n"+ex);
+                log.writeLog(ex.getMessage());                
+            } catch (IOException ex1) {
+                JOptionPane.showMessageDialog(null,"Ошибка записи в лог файл\n"+ex1);                
+            }
+        }        
+    }//GEN-LAST:event_jButton2ActionPerformed
 
     /**
      * @param args the command line arguments
@@ -153,6 +359,7 @@ public class JFScanner extends javax.swing.JFrame {
     private javax.swing.JButton jButton1;
     private javax.swing.JButton jButton2;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTextArea jTextArea1;
